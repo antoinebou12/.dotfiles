223fdbe0da0fb34e9f8d0cd84ea2d94a4dae1a81
Antoine Boucher <antoine.bou13@gmail.com>
2023-07-22 02:55:56 -0400

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -39,7 +39,62 @@ uninstall_jdk() {
     fi
 }
 
+# @description Uninstall Python
+uninstall_python() {
+    echo "Uninstalling Python..."
+
+    # Check if Python is installed
+    if type -p python > /dev/null; then
+        echo "Python is installed. Proceeding with uninstallation..."
+
+        # Uninstall Python
+        sudo apt-get purge python3.\*
+        sudo apt-get autoremove
+
+        echo "Python has been uninstalled."
+    else
+        echo "Python is not installed."
+    fi
+}
+
+# @description Uninstall Rust
+uninstall_rust() {
+    echo "Uninstalling Rust..."
+
+    # Check if Rust is installed
+    if type -p rustc > /dev/null; then
+        echo "Rust is installed. Proceeding with uninstallation..."
+
+        # Uninstall Rust
+        rustup self uninstall -y
+
+        echo "Rust has been uninstalled."
+    else
+        echo "Rust is not installed."
+    fi
+}
+
+# @description Uninstall Go
+uninstall_go() {
+    echo "Uninstalling Go..."
+
+    # Check if Go is installed
+    if type -p go > /dev/null; then
+        echo "Go is installed. Proceeding with uninstallation..."
+
+        # Uninstall Go
+        sudo rm -rf /usr/local/go
+
+        echo "Go has been uninstalled."
+    else
+        echo "Go is not installed."
+    fi
+}
+
+
 uninstall_vimrc
 uninstall_tmux
 uninstall_oh_my_zsh
 uninstall_jdk
+uninstall_rust
+uninstall_go
